f4498197ea0f6134a3d7b293de74774139274ce4
patorma <pocontreras@ing.ucsc.cl>
2018-11-12 18:49:18 -0300

    En el Dao se implemento el listar todos los colegios en el archivo EscuelaDaoImpl.java

--- src/main/java/com/patriciocontreras/SistEscuela/app/models/dao/EscuelaDaoImpl.java
+++ src/main/java/com/patriciocontreras/SistEscuela/app/models/dao/EscuelaDaoImpl.java
@@ -2,14 +2,27 @@ package com.patriciocontreras.SistEscuela.app.models.dao;
 
 import java.util.List;
 
-import com.patriciocontreras.SistEscuela.app.models.entity.Escuela;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.patriciocontreras.SistEscuela.app.models.entity.Escuela;
+//se marca la clase como un componente de persistencia acceso a datos
+@Repository
 public class EscuelaDaoImpl implements IEscuelaDao {
 
+	//se inyecta entityManager con la siguiente anotacion
+	@PersistenceContext //contiene la unidad de persistencia
+	private EntityManager em;
+	
+	@SuppressWarnings("unchecked")//se suprime el mensaje warning
+	@Transactional(readOnly=true)//de lectura, transaccional porque es de lectura
 	@Override
 	public List<Escuela> findAll() {
 		// TODO Auto-generated method stub
-		return null;
+		return em.createQuery("from Escuela").getResultList();
 	}
 
 }
