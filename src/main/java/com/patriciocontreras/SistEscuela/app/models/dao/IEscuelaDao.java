db13f8e9ed74fa542b22b132f72dd66668d50e52
patorma <pocontreras@ing.ucsc.cl>
2018-11-14 12:29:07 -0300

    se borro EscuelaDaoImpl.java y se borro los metodos de la interfaz IEscuelaDao

--- src/main/java/com/patriciocontreras/SistEscuela/app/models/dao/IEscuelaDao.java
+++ src/main/java/com/patriciocontreras/SistEscuela/app/models/dao/IEscuelaDao.java
@@ -6,11 +6,5 @@ import com.patriciocontreras.SistEscuela.app.models.entity.Escuela;
 
 public interface IEscuelaDao {
 
-	public List<Escuela> findAll(); //retornar todas las escuelas
 	
-	public void save (Escuela escuela);
-	
-	public Escuela findOne(Long id);//busca por una escuela
-	
-	public void delete(Long id);
 }
